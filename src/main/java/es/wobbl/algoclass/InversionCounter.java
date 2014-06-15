2ef65d4838966556684edc83f19db834a5f02bf6
Matthias Schuetz <mschuetz@gmail.com>
2014-06-15 17:40:59 +0200

    fixed inversion counter. relied on uncommitted file

--- src/main/java/es/wobbl/algoclass/InversionCounter.java
+++ src/main/java/es/wobbl/algoclass/InversionCounter.java
@@ -6,14 +6,11 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import com.google.common.base.Charsets;
 import com.google.common.base.Function;
 import com.google.common.collect.Lists;
 import com.google.common.io.Files;
-
-import de.idlecrew.util.Profile;
 
 public class InversionCounter {
 
@@ -37,10 +34,10 @@ public class InversionCounter {
 		final ArrayList<T> out = Lists.newArrayListWithCapacity(l1.size() + l2.size());
 
 		int j = 0, k = 0;
-		int n = l1.size() + l2.size();
+		final int n = l1.size() + l2.size();
 		for (int i = 0; i < n; i++) {
-			T v1 = j < l1.size() ? l1.get(j) : null;
-			T v2 = k < l2.size() ? l2.get(k) : null;
+			final T v1 = j < l1.size() ? l1.get(j) : null;
+			final T v2 = k < l2.size() ? l2.get(k) : null;
 			if (v1 != null && (v2 == null || v1.compareTo(v2) < 0)) {
 				out.add(v1);
 				j++;
@@ -58,30 +55,31 @@ public class InversionCounter {
 		if (list.size() <= 1)
 			return result(list, 0L);
 
-		Result<T> a = count(binarySplit(list, 0));
-		Result<T> b = count(binarySplit(list, 1));
-		Result<T> c = countSplitInversions(a.list, b.list);
+		final Result<T> a = count(binarySplit(list, 0));
+		final Result<T> b = count(binarySplit(list, 1));
+		final Result<T> c = countSplitInversions(a.list, b.list);
 		return result(c.list, a.inversions + b.inversions + c.inversions);
 	}
-	
+
 	static long calls = 0;
 
 	public static void main(String[] args) throws IOException {
-		final List<Integer> numbers = Lists.transform(Files.readLines(new File(System.getenv("HOME") + File.separator + "IntegerArray.txt"), Charsets.UTF_8),
+		final List<Integer> numbers = Lists.transform(
+				Files.readLines(new File(System.getenv("HOME") + File.separator + "IntegerArray.txt"), Charsets.UTF_8),
 				new Function<String, Integer>() {
 
-					@Override
-					public Integer apply(String input) {
-						return new Integer(input);
-					}
-				});
-		Profile.report("count inversions", TimeUnit.MILLISECONDS, 1, new Runnable() {
 			@Override
-			public void run() {
-				Result<Integer> inversions = count(numbers);
-				System.out.println(inversions.inversions);
-				System.out.println(calls);
+			public Integer apply(String input) {
+				return new Integer(input);
 			}
 		});
+		/*
+		 * Profile.report("count inversions", TimeUnit.MILLISECONDS, 1, new
+		 * Runnable() {
+		 * 
+		 * @Override public void run() { Result<Integer> inversions =
+		 * count(numbers); System.out.println(inversions.inversions);
+		 * System.out.println(calls); } });
+		 */
 	}
 }
